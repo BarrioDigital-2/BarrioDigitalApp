37444be1d2065aa8179c900358f519a90fc39a75
miloQ1 <ca.queupil@duocuc.cl>
2026-09-13 20:07:42 -0300

    primer commit - spring inits

---
+++ ms-barriodigital-requests/src/main/java/cl/duoc/ms_barriodigital_requests/MsBarriodigitalRequestsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_barriodigital_requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBarriodigitalRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBarriodigitalRequestsApplication.class, args);
+	}
+
+}
---
+++ ms-barriodigital-requests/src/test/java/cl/duoc/ms_barriodigital_requests/MsBarriodigitalRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_barriodigital_requests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBarriodigitalRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
